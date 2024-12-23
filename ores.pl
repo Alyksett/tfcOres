d5db8d1b46fdfcea7a11933c27481c19e324e281
Austin Lyksett <alyksett1029@gmail.com>
2024-12-23 00:49:53 -0600

    progress

--- ores.pl
+++ ores.pl
@@ -1,49 +1,47 @@
 :- use_module(library(clpfd)).
 
-/*
-rBottom rTop 10 15 25 30
-*/
-copper(50, 65, _, _, _, _).
-zinc(20, 30, _, _, _, _).
-bismuth(10, 20, _, _, _, _).
-
 max(1000).
 
-find(X, Y, Z,) :-
+find_partial(Copper, Bismuth, Zinc) :-
+    /*Var | Nugget (35 units)| Poor (35 units)| Normal (35 units)| Rich (35 units) */
+    find(Copper, 0, 10, 0, 0, Bismuth, 0, 10, 0, 0, Zinc, 0, 10, 0, 0).
+
+find(Copper,
+ CopperNugget,
+  CopperPoor,
+   CopperNormal,
+    CopperRich,
+     Bismuth,
+      BismuthNugget,
+       BismuthPoor,
+        BismuthNormal,
+         BismuthRich,
+          Zinc,
+           ZincNugget,
+            ZincPoor,
+             ZincNormal,
+              ZincRich) :-
     max(Max),
 
-    X #>= 0, X #=< Max,
-    Y #>= 0, Y #=< Max,
-    Z #>= 0, Z #=< Max,
+    Copper #>= 0, Copper #=< Max,
+    Bismuth #>= 0, Bismuth #=< Max,
+    Zinc #>= 0, Zinc #=< Max,
     
-    X mod 15 #=0,
-    Y mod 15 #=0,
-    Z mod 15 #=0,
+    Copper mod 15 #=0,
+    Bismuth mod 15 #=0,
+    Zinc mod 15 #=0,
     
-    S #= X + Y + Z,
+    S #= Copper + Bismuth + Zinc,
     S #=< Max,
     S mod 100 #=0,
-    copper(cBottom, cTop, n, p, m, r),
-    zinc(zBottom, zTop, n, p, m, r),
-    bismuth(bBottom, bTop, n, p, m, r),
-    
-    /*
-    */
-    
 
-    Xr #= (X * 100) // S,
-    Yr #= (Y * 100) // S,
-    Zr #= (Z * 100) // S,
+    Copperr #= (Copper * 100) // S,
+    Bismuthr #= (Bismuth * 100) // S,
+    Zincr #= (Zinc * 100) // S,
     
-    Xr #>= 50, Xr #=< 65,
-    Yr #>= 20, Yr #=< 30,
-    Zr #>= 10, Zr #=< 20,
+    Copperr #>= 50, Copperr #=< 65,
+    Bismuthr #>= 20, Bismuthr #=< 30,
+    Zincr #>= 10, Zincr #=< 20,
 
-    labeling([], [X, Y, Z]).
+    labeling([], [Copper, Bismuth, Zinc]).
     
-n_factorial(0, 1).
-n_factorial(N, F) :-
-        N #> 0,
-        N1 #= N - 1,
-        F #= N * F1,
-        n_factorial(N1, F1).
