fba6930538b5fc44b89190f6751715f406911766
Austin Lyksett <alyksett1029@gmail.com>
2024-12-24 00:35:04 -0600

    Working!

--- ores.pl
+++ ores.pl
@@ -1,74 +1,105 @@
 :- use_module(library(clpfd)).
 
+/*
+    Constraints for multiples of ores + purities
+
+    Tc = (nu_c*C_nu + po_c*C_p + noc*C_no +  pu_c*C_p)
+    Tb = (nu_b*B_nu + po_b*B_p + nob*B_no +  pu_b*C_p)
+    Tz = (nu_z*Z_nu + po_z*Z_p + noz*Z_no +  pu_z*C_p)
+
+    | nuc < am_nuc, po_c < am_nuc, noc < am_noc, pu_c < am_nuc,    
+    | nub < am_nub, po_b < am_nub, nob < am_nob, pu_b < am_nub,    
+    | nuz < am_nuz, po_z < am_nuz, noz < am_noz, pu_z < am_nuz,    
+
+    Copper:
+        50-65%
+    Bismuth:
+        10-20%
+    Zinc:
+        20-30%
+    and ratios + units
+*/
+
+
 max(200).
 
 find_partial(Copper, Bismuth, Zinc) :-
-    /*Var | Nugget (10 units)| Poor (15 units)| Normal (25 units)| Rich (35 units) */
-    find(Copper, 5, 0, 0, Bismuth, 100, 100, 100, Zinc, 100, 100, 100).
+    /* Nugget (10 units)| Poor (15 units)| Normal (25 units)| Rich (35 units) */
+    find(Copper, 1, 3, 0, 0, Bismuth, 10, 20, 10, 0, Zinc, 10, 20, 10, 0).
 
 find(
     Copper,
     CopperNugget,
     CopperPoor,
     CopperNormal,
+    CopperRich,
     Bismuth,
     BismuthNugget,
     BismuthPoor,
     BismuthNormal,
+    BismuthRich,
     Zinc,
     ZincNugget,
     ZincPoor,
-    ZincNormal
+    ZincNormal,
+    ZincRich
     ) :-
     
-    /*
-
-        THESE ARE THE CONSTRAINTS:
-
-        Tc = (nu_c*C_nu + po_c*C_p + noc*C_no +  pu_c*C_p)
-        Tb = (nu_b*B_nu + po_b*B_p + nob*B_no +  pu_b*C_p)
-        Tz = (nu_z*Z_nu + po_z*Z_p + noz*Z_no +  pu_z*C_p)
-
-        | nuc < am_nuc, po_c < am_nuc, noc < am_noc, pu_c < am_nuc,    
-        | nub < am_nub, po_b < am_nub, nob < am_nob, pu_b < am_nub,    
-        | nuz < am_nuz, po_z < am_nuz, noz < am_noz, pu_z < am_nuz,    
+    CopperNuggetRange #>= 0, CopperNuggetRange #=< CopperNugget,
+    CopperPoorRange #>= 0, CopperPoorRange #=< CopperPoor,
+    CopperNormalRange #>= 0, CopperNormalRange #=< CopperNormal,
+    CopperRichRange #>= 0, CopperRichRange #=< CopperRich,
+    Copper #= (CopperNuggetRange * 10 + CopperPoorRange * 15 + CopperNormalRange * 25 + CopperRichRange * 35),
     
-        and ratios + units
-    */
-
-    Copper #= (CopperNuggetAmount * 10 + CopperPoorAmount * 15),
-    CopperNuggetAmount #=< CopperNugget, CopperNuggetAmount #>= 0,
-    CopperPoorAmount #=< CopperPoor, CopperPoorAmount #>= 0,
-     
-    Bismuth #= (BismuthNuggetAmount * 10 + BismuthPoorAmount * 15),
-    BismuthNuggetAmount #=< BismuthNugget, BismuthNuggetAmount #>= 0,
-    BismuthPoorAmount #=< BismuthPoor, BismuthPoorAmount #>= 0,
-
-    Zinc #= (ZincNuggetAmount * 10 + ZincPoorAmount * 15),
-    ZincNuggetAmount #=< ZincNugget, ZincNuggetAmount #>= 0,
-    ZincPoorAmount #=< ZincPoor, ZincPoorAmount #>= 0, 
+    BismuthNuggetRange #>= 0, BismuthNuggetRange #=< BismuthNugget, 
+    BismuthPoorRange #>= 0, BismuthPoorRange #=< BismuthPoor,
+    BismuthNormalRange #>= 0, BismuthNormalRange #=< BismuthNormal,
+    BismuthRichRange #>= 0, BismuthRichRange #=< BismuthRich,
+    Bismuth #= (BismuthNuggetRange * 10 + BismuthPoorRange * 15 + BismuthNormalRange * 25 + BismuthRichRange * 35),
     
-
+    ZincNuggetRange #>= 0, ZincNuggetRange #=< ZincNugget,
+    ZincPoorRange #>= 0, ZincPoorRange #=< ZincPoor,
+    ZincNormalRange #>= 0, ZincNormalRange #=< ZincNormal,
+    ZincRichRange #>= 0, ZincRichRange #=< ZincRich,
+    Zinc #= (ZincNuggetRange * 10 + ZincPoorRange * 15 + ZincNormalRange * 25 + ZincRichRange * 35),
+    
+    
     max(MaxLimit),
-
+    
     Copper #>= 0, Copper #=< MaxLimit,
     Bismuth #>= 0, Bismuth #=< MaxLimit,
     Zinc #>= 0, Zinc #=< MaxLimit,
-    
-   
 
+    S #= Copper + Bismuth + Zinc,
+    S #> 0,
     S #=< MaxLimit,
     S mod 100 #=0,
-    S #= Copper + Bismuth + Zinc,
    
     Copperr #= (Copper * 100) // S,
     Bismuthr #= (Bismuth * 100) // S,
     Zincr #= (Zinc * 100) // S,
     
     Copperr #>= 50, Copperr #=< 65,
-    Bismuthr #>= 20, Bismuthr #=< 30,
-    Zincr #>= 10, Zincr #=< 20,
+    Bismuthr #>= 10, Bismuthr #=< 20,
+    Zincr #>= 20, Zincr #=< 30,
+
+    % fudge the solver to bind vars to values? Need this
+    labeling([], [Copper, Bismuth, Zinc, CopperNugget, CopperPoor, CopperNormal, BismuthNugget, BismuthPoor, BismuthNormal, ZincNugget, ZincPoor, ZincNormal, ZincNuggetRange, ZincPoorRange, ZincNormalRange, ZincRichRange]),
+
+
+    write('Copper Nuggets: '), write(CopperNuggetRange), nl,
+    write('Copper Poor: '), write(CopperPoorRange), nl,
+    write('Copper Normal: '), write(CopperNormalRange), nl,
+    write('Copper Rich: '), write(CopperRichRange), nl,
+    write('Bismuth Nugget: '), write(BismuthNuggetRange), nl,
+    write('Bismuth Poor: '), write(BismuthPoorRange), nl,
+    write('Bismuth Normal: '), write(BismuthNormalRange), nl,
+    write('Bismuth Rich: '), write(BismuthRichRange), nl,
+    write('Zinc Nugget: '), write(ZincNuggetRange), nl,
+    write('Zinc Poor: '), write(ZincPoorRange), nl,
+    write('Zinc Normal: '), write(ZincNormalRange), nl,
+    write('Zinc Rich: '), write(ZincRichRange), nl.
+
     
-    labeling([], [Copper, Bismuth, Zinc, CopperNugget, CopperPoor, CopperNormal, BismuthNugget, BismuthPoor, BismuthNormal,  ZincNugget, ZincPoor, ZincNormal]).
 
 
